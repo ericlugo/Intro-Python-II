5866812e8b57a879c7ede2ced753454b7ab44831
Eric SarragaLugo <ericlugo@users.noreply.github.com>
2020-05-18 01:56:08 -0400

    day2 mvp complete

--- src/player.py
+++ src/player.py
@@ -1,17 +1,35 @@
-# Write a class to hold player information, e.g. what room they are in
-# currently.
-
-
 class Player:
-    def __init__(self, room):
-        self.room = room
+    def __init__(self, current_room, items=[]):
+        self.current_room = current_room
+        self.items = items
 
     def get_location(self):
-        return self.room
+        return self.current_room
 
     def set_location(self, room):
         if room is None:
-            return 'You are unable to proceed in that direction.'
+            return '> You are unable to proceed in that direction.'
         else:
-            self.room = room
-            return f'You have successfully moved.'
+            self.current_room = room
+
+    def check_items(self):
+        item_names = []
+        for item in self.items:
+            item_names.append(" ".join(item.name.split("_")).capitalize())
+        return item_names
+
+    def get_item(self, item_name):
+        for index, item in enumerate(self.items):
+            if "_".join(item_name.split()) == item.name:
+                retrieved_item = item
+                self.items.pop(index)
+                retrieved_item.on_drop()
+                return retrieved_item
+        return None
+
+    def give_item(self, new_item):
+        if new_item is not None:
+            self.items.append(new_item)
+            new_item.on_take()
+        else:
+            return '> I cannot take what is not.'
--- src/room.py
+++ src/room.py
@@ -1,15 +1,12 @@
-# Implement a class to hold room information. This should have name and
-# description attributes.
-
-
 class Room:
-    def __init__(self, name, description):
+    def __init__(self, name, description, items=[]):
         self.name = name
         self.description = description
         self.n_to = None
         self.s_to = None
         self.e_to = None
         self.w_to = None
+        self.items = items
 
     def __str__(self):
         return (f"""You are currently in the {self.name}\n{self.description}""")
@@ -25,3 +22,23 @@ class Room:
 
     def to_w(self):
         return self.w_to
+
+    def check_items(self):
+        item_names = []
+        for item in self.items:
+            item_names.append(" ".join(item.name.split("_")).capitalize())
+        return item_names
+
+    def get_item(self, item_name):
+        for index, item in enumerate(self.items):
+            if "_".join(item_name.split()) == item.name:
+                retrieved_item = item
+                self.items.pop(index)
+                return retrieved_item
+        return None
+
+    def give_item(self, new_item):
+        if new_item is not None:
+            self.items.append(new_item)
+        else:
+            return '> I cannot drop what I do not have.'
